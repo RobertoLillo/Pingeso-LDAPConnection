559594af1f3585a9f1292abadebe4df7959881d0
RobertoLillo <roberto.lillo@usach.cl>
2020-12-01 21:58:10 -0300

    Actualizacion en proceso de Bind

--- ldapConnection.js
+++ ldapConnection.js
@@ -1,5 +1,12 @@
+// DC = Domain Controller, servidor de autenticación.
+// DN = Distinguished Name, dirección del usuario en el árbol.
+// CN = Common Name, nombre del usuario.
+// SN = Surname, apellido del usuario.
+// sAMAccountName = nombre de la cuenta.
+
 const ldapjs = require('ldapjs');
 
+// Búsqueda
 function searchUser(sAMAccountName) {
     const opts = {
         filter: '(sAMAccountName=' + sAMAccountName + ')',
@@ -7,27 +14,45 @@ function searchUser(sAMAccountName) {
         attributes: ['cn', 'memberOf']
     };
 
+    // Bind, proceso para establecer conexión con el directorio.
+    client.bind(credentials.dn, credentials.pass, function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Bind success!\n");
+        }
+    });
+
     client.search(credentials.dc, opts, function (err, res) {
         if (err) {
             console.log("Search error.");
         } else {
             res.on('searchEntry', function (entry) {
-                console.log('Entry: ', JSON.stringify(entry.object));
+                console.log("Nombre:\t", entry.object.cn);
+                console.log("Grupo:\t", entry.object.memberOf);
             });
             res.on('error', function (err) {
                 console.log('Error: ', err.message);
+            });
+
+            // Unbind, proceso para terminar la conexión con el directorio.
+            client.unbind(function (err) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log("\nUnbind success!");
+                }
             });
         };
     });
 };
 
-/* ------------------------------------------------------------------- */
-
-// DC = Domain Controller, servidor de autenticación.
-// DN = Distinguished Name, dirección del usuario en el árbol.
-// CN = Common Name, nombre del usuario.
-// SN = Surname, apellido del usuario.
-// sAMAccountName = nombre de la cuenta.
+// Credenciales para el Bind.
+const credentials = {
+    dc: 'DC=samba,DC=lan',
+    dn: 'CN=Administrator,CN=Users,DC=samba,DC=lan',
+    pass: 'Pingeso1*'
+};
 
 // Cliente LDAP, necesario para conectarse al directorio.
 const client = ldapjs.createClient({
@@ -37,20 +62,4 @@ const client = ldapjs.createClient({
     }
 });
 
-// Credenciales para el Bind.
-const credentials = {
-    dc: 'DC=samba,DC=lan',
-    dn: 'CN=Administrator,CN=Users,DC=samba,DC=lan',
-    pass: 'Pingeso1*'
-};
-
-// Bind, proceso para establecer conexión con el directorio.
-client.bind(credentials.dn, credentials.pass, function (err) {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log("Bind success!");
-    }
-});
-
-console.log(searchUser('roberto.lillo@usach.cl'));
+searchUser('roberto.lillo@usach.cl');
