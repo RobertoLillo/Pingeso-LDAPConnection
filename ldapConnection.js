7d15f0281887cec93dbdb13a1748532c5381dd8c
RobertoLillo <roberto.lillo@usach.cl>
2020-12-29 15:04:10 -0300

    Funcion asincrona

--- ldapConnection.js
+++ ldapConnection.js
@@ -6,60 +6,60 @@
 
 const ldapjs = require('ldapjs');
 
-// Búsqueda
 function searchUser(sAMAccountName) {
-    const opts = {
-        filter: '(sAMAccountName=' + sAMAccountName + ')',
-        scope: 'sub',
-        attributes: ['cn', 'memberOf']
-    };
-
-    // Bind, proceso para establecer conexión con el directorio.
-    client.bind(credentials.dn, credentials.pass, function (err) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log("Bind success!\n");
-        }
-    });
-
-    client.search(credentials.dc, opts, function (err, res) {
-        if (err) {
-            console.log("Search error.");
-        } else {
-            res.on('searchEntry', function (entry) {
-                console.log("Nombre:\t", entry.object.cn);
-                console.log("Grupo:\t", entry.object.memberOf);
-            });
-            res.on('error', function (err) {
-                console.log('Error: ', err.message);
-            });
-
-            // Unbind, proceso para terminar la conexión con el directorio.
-            client.unbind(function (err) {
-                if (err) {
-                    console.log(err);
-                } else {
-                    console.log("\nUnbind success!");
-                }
-            });
+    return new Promise((resolve, reject) => {
+        // Credenciales para el Bind.
+        const credentials = {
+            dc: 'DC=samba,DC=lan',
+            dn: 'CN=Administrator,CN=Users,DC=samba,DC=lan',
+            pass: 'Pingeso1*'
         };
+
+        // Opciones de búsqueda.
+        const opts = {
+            filter: `(sAMAccountName=${sAMAccountName})`,
+            scope: 'sub',
+            attributes: ['cn', 'memberOf']
+        };
+
+        // Cliente LDAP, necesario para conectarse al directorio.
+        const client = ldapjs.createClient({
+            url: 'ldaps://10.0.2.15',
+            tlsOptions: {
+                rejectUnauthorized: false
+            }
+        });
+
+        client.bind(credentials.dn, credentials.pass, (err) => {
+            if (err) {
+                return err;
+            }
+            const results = [];
+            client.search(credentials.dc, opts, (err, res) => {
+                res.on('searchEntry', (entry) => {
+                    results.push(entry.object);
+                });
+                res.on('error', (error) => {
+                    console.log(`Error: ${error.message}`);
+                    if (err) {
+                        client.destroy();
+                        reject(err);
+                    }
+                });
+                res.on('end', (result) => {
+                    console.log(`Search_Result: ${result.status}`);
+                    client.destroy();
+                    resolve(results);
+                });
+            });
+        });
     });
-};
+}
 
-// Credenciales para el Bind.
-const credentials = {
-    dc: 'DC=samba,DC=lan',
-    dn: 'CN=Administrator,CN=Users,DC=samba,DC=lan',
-    pass: 'Pingeso1*'
-};
+async function search(name) {
+    const result = await searchUser(name);
+    console.log(result);
+    return result;
+}
 
-// Cliente LDAP, necesario para conectarse al directorio.
-const client = ldapjs.createClient({
-    url: 'ldaps://10.0.2.15',
-    tlsOptions: {
-        rejectUnauthorized: false
-    }
-});
-
-searchUser('roberto.lillo@usach.cl');
+search('roberto.lillo@usach.cl');
